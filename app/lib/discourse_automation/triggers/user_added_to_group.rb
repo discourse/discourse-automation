ba5564f6446d46ee30b1ba305610613df252da4d
jjaffeux <j.jaffeux@gmail.com>
2021-01-21 08:50:37 +0100

    removes on_update of user_added_to_group

--- app/lib/discourse_automation/triggers/user_added_to_group.rb
+++ app/lib/discourse_automation/triggers/user_added_to_group.rb
@@ -3,6 +3,4 @@
 DiscourseAutomation::Triggerable::USER_ADDED_TO_GROUP = 'user_added_to_group'
 
 DiscourseAutomation::Triggerable.add(DiscourseAutomation::Triggerable::USER_ADDED_TO_GROUP) do
-  on_update do |automation, metadata|
-  end
 end
