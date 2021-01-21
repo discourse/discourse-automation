0f5e66e5de752af2be926778f73d4d50c793804c
jjaffeux <j.jaffeux@gmail.com>
2021-01-21 08:46:18 +0100

    can't mutate frozen

--- app/lib/discourse_automation/scriptable.rb
+++ app/lib/discourse_automation/scriptable.rb
@@ -73,6 +73,7 @@ module DiscourseAutomation
         map[:site_title] = SiteSetting.title
 
         map.each do |key, value|
+          input = input.gsub("%%#{key.upcase}%%", value)
         end
 
         input
