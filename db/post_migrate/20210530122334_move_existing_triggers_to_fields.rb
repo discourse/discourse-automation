d05453fcda70b9470cbe4b8daa6f11b223e24fa5
jjaffeux <j.jaffeux@gmail.com>
2021-06-01 15:02:47 +0200

    uses raw sql instead of ar for migrations

--- db/post_migrate/20210530122334_move_existing_triggers_to_fields.rb
+++ db/post_migrate/20210530122334_move_existing_triggers_to_fields.rb
@@ -2,17 +2,19 @@
 
 class MoveExistingTriggersToFields < ActiveRecord::Migration[6.1]
   def create_field(automation, component, name, metadata)
-    automation.fields.create!(
-      component: component,
-      name: name,
-      metadata: metadata,
-      target: 'trigger'
-    )
+    DB.exec(<<~SQL, automation_id: automation.id, component: component, name: name, metadata: metadata.to_json, created_at: Time.zone.now)
+      INSERT INTO discourse_automation_fields (automation_id, component, name, metadata, target, created_at, updated_at)
+      VALUES (:automation_id, :component, :name, :metadata, 'trigger', :created_at, :created_at)
+    SQL
   end
 
   def change
     DB.query('SELECT name,automation_id,metadata FROM discourse_automation_triggers').each do |trigger|
       automation = DiscourseAutomation::Automation.find(trigger.automation_id)
+
+      if trigger.name == 'point-in-time'
+        trigger.name = 'point_in_time'
+      end
 
       automation.update_column(:trigger, trigger.name)
 
@@ -21,7 +23,7 @@ class MoveExistingTriggersToFields < ActiveRecord::Migration[6.1]
           create_field(automation, 'group', 'joined_group', { group_id: value })
         end
 
-        if key == 'execute_at' && trigger.name == 'point_in_time'
+        if key == 'execute_at' && trigger.name == 'point-in-time'
           create_field(automation, 'date', 'execute_at', { date: value })
         end
 
