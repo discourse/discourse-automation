07b8e071e1ab03804bf4e6fb65bcd79c849da309
jjaffeux <j.jaffeux@gmail.com>
2021-06-01 22:15:03 +0200

    do not use AR in migrations

--- db/post_migrate/20210530122334_move_existing_triggers_to_fields.rb
+++ db/post_migrate/20210530122334_move_existing_triggers_to_fields.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class MoveExistingTriggersToFields < ActiveRecord::Migration[6.1]
-  def create_field(automation, component, name, metadata)
-    DB.exec(<<~SQL, automation_id: automation.id, component: component, name: name, metadata: metadata.to_json, created_at: Time.zone.now)
+  def create_field(automation_id, component, name, metadata)
+    DB.exec(<<~SQL, automation_id: automation_id, component: component, name: name, metadata: metadata.to_json, created_at: Time.zone.now)
       INSERT INTO discourse_automation_fields (automation_id, component, name, metadata, target, created_at, updated_at)
       VALUES (:automation_id, :component, :name, :metadata, 'trigger', :created_at, :created_at)
     SQL
@@ -10,29 +10,31 @@ class MoveExistingTriggersToFields < ActiveRecord::Migration[6.1]
 
   def change
     DB.query('SELECT name,automation_id,metadata FROM discourse_automation_triggers').each do |trigger|
-      automation = DiscourseAutomation::Automation.find(trigger.automation_id)
-
       if trigger.name == 'point-in-time'
         trigger.name = 'point_in_time'
       end
 
-      automation.update_column(:trigger, trigger.name)
+      DB.exec(<<~SQL, automation_id: trigger.automation_id, trigger: trigger.name)
+        UPDATE discourse_automation_automations
+        SET trigger = :trigger
+        WHERE id = :automation_id
+      SQL
 
       trigger.metadata.each do |key, value|
         if key == 'group_ids' && trigger.name == 'user_added_to_group'
-          create_field(automation, 'group', 'joined_group', { group_id: value })
+          create_field(trigger.automation_id, 'group', 'joined_group', { group_id: value })
         end
 
-        if key == 'execute_at' && trigger.name == 'point-in-time'
-          create_field(automation, 'date', 'execute_at', { date: value })
+        if key == 'execute_at' && trigger.name == 'point_in_time'
+          create_field(trigger.automation_id, 'date', 'execute_at', { date: value })
         end
 
         if key == 'category_id' && trigger.name == 'post_created_edited'
-          create_field(automation, 'category', 'restricted_category', { category_id: value })
+          create_field(trigger.automation_id, 'category', 'restricted_category', { category_id: value })
         end
 
         if key == 'topic' && trigger.name == 'topic'
-          create_field(automation, 'topic', 'restricted_topic', { topic_id: value })
+          create_field(trigger.automation_id, 'topic', 'restricted_topic', { topic_id: value })
         end
       end
     end
