1fbe8047052387beabb7037d2f2d48a194530bfd
jjaffeux <j.jaffeux@gmail.com>
2021-10-18 12:43:31 +0200

    implements custom automation API

--- spec/integration/core_ext_spec.rb
+++ spec/integration/core_ext_spec.rb
@@ -3,75 +3,90 @@
 require_relative '../discourse_automation_helper'
 
 describe 'Core extensions' do
+  fab!(:automation_1) { Fabricate(:automation) }
+  fab!(:automation_2) { Fabricate(:automation) }
+
   describe 'post custom fields' do
     it 'supports discourse_automation_ids' do
       post = create_post
-      post.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
+      automation_1.attach_custom_field(post)
 
-      expect(post.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1])
+      expect(post.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id])
 
-      post.create_singular(DiscourseAutomation::CUSTOM_FIELD, 2, [:integer])
+      automation_2.attach_custom_field(post)
 
-      expect(post.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1, 2])
+      expect(post.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id, automation_2.id])
 
       PostCustomField.where(post_id: post.id, name: DiscourseAutomation::CUSTOM_FIELD).delete_all
 
       expect(post.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to be(nil)
 
-      post.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      post.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      post.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      post.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
+      automation_1.attach_custom_field(post)
+      automation_1.attach_custom_field(post)
+      automation_1.attach_custom_field(post)
+      automation_1.attach_custom_field(post)
 
-      expect(post.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1])
+      expect(post.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id])
+
+      automation_1.detach_custom_field(post)
+
+      expect(post.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to be(nil)
     end
   end
 
   describe 'topic custom fields' do
     it 'supports discourse_automation_ids' do
       topic = create_topic
-      topic.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
+      automation_1.attach_custom_field(topic)
 
-      expect(topic.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1])
+      expect(topic.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id])
 
-      topic.create_singular(DiscourseAutomation::CUSTOM_FIELD, 2, [:integer])
+      automation_2.attach_custom_field(topic)
 
-      expect(topic.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1, 2])
+      expect(topic.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id, automation_2.id])
 
       TopicCustomField.where(topic_id: topic.id, name: DiscourseAutomation::CUSTOM_FIELD).delete_all
 
       expect(topic.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to be(nil)
 
-      topic.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      topic.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      topic.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      topic.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
+      automation_1.attach_custom_field(topic)
+      automation_1.attach_custom_field(topic)
+      automation_1.attach_custom_field(topic)
+      automation_1.attach_custom_field(topic)
 
-      expect(topic.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1])
+      expect(topic.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id])
+
+      automation_1.detach_custom_field(topic)
+
+      expect(topic.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to be(nil)
     end
   end
 
   describe 'user custom fields' do
     it 'supports discourse_automation_ids' do
       user = create_user
-      user.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
+      automation_1.attach_custom_field(user)
 
-      expect(user.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1])
+      expect(user.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id])
 
-      user.create_singular(DiscourseAutomation::CUSTOM_FIELD, 2, [:integer])
+      automation_2.attach_custom_field(user)
 
-      expect(user.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1, 2])
+      expect(user.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id, automation_2.id])
 
       UserCustomField.where(user_id: user.id, name: DiscourseAutomation::CUSTOM_FIELD).delete_all
 
       expect(user.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to be(nil)
 
-      user.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      user.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      user.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
-      user.create_singular(DiscourseAutomation::CUSTOM_FIELD, 1, [:integer])
+      automation_1.attach_custom_field(user)
+      automation_1.attach_custom_field(user)
+      automation_1.attach_custom_field(user)
+      automation_1.attach_custom_field(user)
 
-      expect(user.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([1])
+      expect(user.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to eq([automation_1.id])
+
+      automation_1.detach_custom_field(user)
+
+      expect(user.reload.custom_fields[DiscourseAutomation::CUSTOM_FIELD]).to be(nil)
     end
   end
 end
