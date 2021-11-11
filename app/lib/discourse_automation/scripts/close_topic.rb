0f27efcdb28d4d6e8502c2f423f3c92f5fecece6
Leonardo Mosquera <ldmosquera@gmail.com>
2021-11-11 17:01:42 +0000

    FEATURE: add close_topic script
    With support for an optional specific user to close the topic with, and
    optional closing message.

---
+++ app/lib/discourse_automation/scripts/close_topic.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+DiscourseAutomation::Scriptable::CLOSE_TOPIC = 'close_topic'
+
+DiscourseAutomation::Scriptable.add(DiscourseAutomation::Scriptable::CLOSE_TOPIC) do
+  field :topic_to_be_closed, component: :text, required: true
+  field :message, component: :text
+  field :user, component: :user
+
+  version 1
+
+  # FIXME: copied over from pin_topic; needs some thought
+  triggerables [:point_in_time]
+
+  script do |_context, fields|
+    next unless topic_id = fields.dig('topic_to_be_closed', 'value')
+    next unless topic = Topic.find_by(id: topic_id)
+
+    message = fields.dig('message', 'value')
+    username = fields.dig('user', 'value') || Discourse.system_user.username
+
+    utils.close_topic(topic, username: username, message: message)
+  end
+end
