1c66402487954ade3a5408b3b53822d06a829e3f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2021-12-01 10:09:01 +0100

    drop constant

--- app/lib/discourse_automation/triggers/stalled_topic.rb
+++ app/lib/discourse_automation/triggers/stalled_topic.rb
@@ -4,12 +4,12 @@ DiscourseAutomation::Triggerable::STALLED_TOPIC = 'stalled_topic'
 
 key = 'discourse_automation.triggerables.stalled_topic.durations'
 ids = %w[PT1H P1D P1W P2W P1M P3M P6M P1Y]
-DURATION_CHOICES = ids.map { |id| { id: id, name: "#{key}.#{id}" } }
+duration_choices = ids.map { |id| { id: id, name: "#{key}.#{id}" } }
 
 DiscourseAutomation::Triggerable.add(DiscourseAutomation::Triggerable::STALLED_TOPIC) do
   field :categories, component: :categories
   field :tags, component: :tags
-  field :stalled_after, component: :choices, extra: { content: DURATION_CHOICES }, required: true
+  field :stalled_after, component: :choices, extra: { content: duration_choices }, required: true
 
   placeholder :topic_url
 end
