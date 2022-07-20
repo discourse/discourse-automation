010c5788523f351fbe36625a0f52d143d5c0286e
Vinoth Kannan <svkn.87@gmail.com>
2022-07-21 04:59:07 +0530

    DEV: action `post_checked` already moved to dedicated controller.

--- app/controllers/discourse_automation/automations_controller.rb
+++ app/controllers/discourse_automation/automations_controller.rb
@@ -10,19 +10,5 @@ module DiscourseAutomation
       automation.trigger!(params.merge(kind: DiscourseAutomation::Triggerable::API_CALL))
       render json: success_json
     end
-
-    def post_checked
-      post = Post.find(params[:post_id])
-      topic = post.topic
-      raise Discourse::NotFound if topic.blank?
-
-      topic.custom_fields[DiscourseAutomation::TOPIC_LAST_CHECKED_BY] = current_user.username
-      topic.custom_fields[DiscourseAutomation::TOPIC_LAST_CHECKED_AT] = Time.zone.now.to_s
-      topic.save_custom_fields
-
-      post.rebake!
-
-      render json: success_json
-    end
   end
 end
