75502e58ba48b1355b90a584ac1ed1f8ee3801ec
davidbattersby <info@davidbattersby.com>
2022-12-21 00:53:47 +0800

    add additional tests for scheduled explorer queries

--- spec/scripts/scheduled_explorer_query_spec.rb
+++ spec/scripts/scheduled_explorer_query_spec.rb
@@ -4,53 +4,61 @@ require_relative '../discourse_automation_helper'
 
 describe 'ScheduledExplorerQuery' do
   fab!(:automation) { Fabricate(:automation, script: DiscourseAutomation::Scriptable::SCHEDULED_EXPLORER_QUERY, trigger: 'recurring') }
+  fab!(:admin_1) { Fabricate(:user, username: "johndoe", admin: true) }
+  fab!(:user_1) { Fabricate(:user, username: "testuser1", admin: false) }
+  let!(:group) { Group.find_by(name: "admins") }
+  let!(:query) { DataExplorer::Query.find(-7) }
+  let!(:recipients) { ["johndoe", "john@doe.com"] }
+  let!(:pm_title) { "Scheduled Report for #{query.name}" }
 
   before do
+    SiteSetting.data_explorer_enabled = true
     SiteSetting.discourse_automation_enabled = true
 
-    automation.upsert_field!('sender', 'user', { value: Discourse.system_user.username }, target: 'trigger')
-    automation.upsert_field!('receiver', 'user', { value: Discourse.system_user.username }, target: 'trigger')
-    automation.upsert_field!('group_id', 'group', { value: 1 }, target: 'trigger')
-    automation.upsert_field!('query_id', 'choices', { value: -7 }, target: 'trigger')
+    automation.upsert_field!('query_id', 'choices', { value: "-7" }, target: 'script')
+    automation.upsert_field!('recipients', 'email_group_user', { value: recipients })
+    automation.upsert_field!('query_params', 'key-value', { value: ['months', '1'] })
+    automation.upsert_field!('recurrence', 'period', { value: { interval: 1, frequency: 'month' } }, target: 'trigger')
+    automation.upsert_field!('start_date', 'date_time', { value: 5.minutes.ago }, target: 'trigger')
   end
 
-  context 'when recurring interval reached then trigger' do
-    fab!(:admin_1) { Fabricate(:user, admin: true) }
-    fab!(:admin_2) { Fabricate(:user, admin: true) }
+  context 'when using recurring trigger' do
+    # before do
+    #   group.add(user_2)
+    # end
 
-    before do
-      start_date = Time.now
-      automation.upsert_field!('recurrence', 'period', { value: { interval: 1, frequency: 'month' }}, target: 'trigger')
-      automation.upsert_field!('start_date', 'date_time', { value: start_date }, target: 'trigger')
+    it 'sends the pm at recurring date_date' do
+      freeze_time 1.month.from_now do
+        expect {
+          Jobs::DiscourseAutomationTracker.new.execute
+          expect(Post.last.topic.title).to eq(pm_title)
+        }.to change { Post.count }.by(1)
+      end
+    end
+
+    it 'should not send to email addresses if creator lacks send to email permission' do
+      expect(Guardian.new(user_1).can_send_private_messages_to_email?).to eq(false)
+
+      automation.update(last_updated_by_id: user_1.id)
+      expect(automation.trigger!).to eq(["johndoe"])
+    end
+
+    it 'should ensure that script creator with send to email permissions, can send pms to emails' do
+      automation.update(last_updated_by_id: Discourse.system_user.id)
+      expect(automation.trigger!).to eq(["johndoe", "john@doe.com"])
+    end
+
+    it 'should ensure admin users receive reports via pm' do
+      expect(Guardian.new(admin_1).user_can_access_query?(query)).to eq(true)
+      total_posts = Post.count
+
+      automation.update(last_updated_by_id: admin_1.id)
       automation.trigger!
+      expect(Post.count).to eq(total_posts + 1)
     end
 
-    it 'report should be received by PM' do
-      post = Post.last
-      query = Query.find(-7)
-      expect(post.topic.title).to eq("Scheduled Report for #{query.name}")
+    it 'should pass guardian clause for both user and group have query access' do
+      expect(Guardian.new(admin_1).group_and_user_can_access_query?(group, query)).to eq(true)
     end
   end
-
-  # context 'when run from user_added_to_group trigger' do
-  #   fab!(:user_1) { Fabricate(:user) }
-  #   fab!(:tracked_group_1) { Fabricate(:group) }
-
-  #   before do
-  #     automation.update!(trigger: 'user_added_to_group')
-  #     automation.upsert_field!('joined_group', 'group', { value: tracked_group_1.id }, target: 'trigger')
-  #   end
-
-  #   it 'creates expected PM' do
-  #     expect {
-  #       tracked_group_1.add(user_1)
-
-  #       post = Post.last
-  #       expect(post.topic.title).to eq("A message from #{Discourse.system_user.username}")
-  #       expect(post.raw).to eq("This is a message sent to @#{user_1.username}")
-  #       expect(post.topic.topic_allowed_users.exists?(user_id: user_1.id)).to eq(true)
-  #       expect(post.topic.topic_allowed_users.exists?(user_id: Discourse.system_user.id)).to eq(true)
-  #     }.to change { Post.count }.by(1)
-  #   end
-  # end
 end
