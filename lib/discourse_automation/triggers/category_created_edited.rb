a2a1012f4f7115185ef485a03f84cee0f02a56d3
Osama Sayegh <asooomaasoooma90@gmail.com>
2024-04-03 18:22:21 +0300

    DEV: Move plugin to discourse core (#264)
    Core commit that moves the plugin to core: https://github.com/discourse/discourse/commit/3d4faf327233817f0e088bcb8a0653984154b5e1

--- lib/discourse_automation/triggers/category_created_edited.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-DiscourseAutomation::Triggerable::CATEGORY_CREATED_EDITED = "category_created_edited"
-
-DiscourseAutomation::Triggerable.add(DiscourseAutomation::Triggerable::CATEGORY_CREATED_EDITED) do
-  field :restricted_category, component: :category
-end
