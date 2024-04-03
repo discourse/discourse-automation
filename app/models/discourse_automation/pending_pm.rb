46a2f9d8f1a7bfa3dd5c49baba76886e8cd45e48
OsamaSayegh <asooomaasoooma90@gmail.com>
2024-04-03 03:45:30 +0300

    DEV: Move plugin to discourse core

--- app/models/discourse_automation/pending_pm.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module DiscourseAutomation
-  class PendingPm < ActiveRecord::Base
-    self.table_name = "discourse_automation_pending_pms"
-
-    belongs_to :automation, class_name: "DiscourseAutomation::Automation"
-  end
-end
--- app/models/discourse_automation/user_global_notice.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module DiscourseAutomation
-  class UserGlobalNotice < ActiveRecord::Base
-    self.table_name = "discourse_automation_user_global_notices"
-
-    belongs_to :user
-  end
-end
